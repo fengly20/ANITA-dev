daa7441ba067b37443eb50030a66eebb952dea79
Leyang Feng <feng@american.edu>
2018-04-16 15:28:46 -0400

    Updated the function

--- code/flactuationMat.m
+++ code/flactuationMat.m
@@ -3,7 +3,9 @@ function map_mat = flactuationMat(date,results_cells,metrics_cells,cum_rise_thre
 value_mat = valueChangeMat(-9999,date,'value',metrics_cells);
 normrise_mat = normRiseMat(results_cells);
 
-map_mat = value_mat.*(normrise_mat<cum_rise_thres);
+map_mat = value_mat;
+map_mat(normrise_mat<cum_rise_thres) = NaN;
+
 end
 
 
