8cdbd09e7a8844d5e2eae5470c54892a857a7df9
Leyang Feng <feng@american.edu>
2018-04-30 15:04:16 -0400

    Added user_min_segs_set in the for loop

--- code/utility_funs/genParamMat.m
+++ code/utility_funs/genParamMat.m
@@ -1,4 +1,9 @@
-function param_mat = genParamMat(pct_set,filt_dist_set,bail_thresh_set,penalty_set,max_complex_set,noise_thresh_set)
+function param_mat = genParamMat(pct_set,filt_dist_set,bail_thresh_set,...
+    penalty_set,max_complex_set,noise_thresh_set,user_min_segs_set)
+
+if nargin<8
+    user_min_segs_set = [1];
+end
 
 param_mat = [];
  for pct_it = 1:length(pct_set)
@@ -7,7 +12,13 @@ param_mat = [];
              for pen_it = 1:length(penalty_set)
                  for mc_it = 1:length(max_complex_set)
                      for nt_it = 1:length(noise_thresh_set)
-                         param_mat = [param_mat; pct_set(pct_it) filt_dist_set(filt_it) bail_thresh_set(bt_it) penalty_set(pen_it) max_complex_set(mc_it) noise_thresh_set(nt_it)];
+                         for um_it = 1:length(user_min_segs_set)
+                             param_mat = [param_mat; ...
+                                 pct_set(pct_it) filt_dist_set(filt_it) ...
+                                 bail_thresh_set(bt_it) penalty_set(pen_it) ...
+                                 max_complex_set(mc_it) noise_thresh_set(nt_it) ...
+                                 user_min_segs_set(um_it)];
+                         end
                      end
                  end
              end
