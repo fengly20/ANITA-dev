787585b9ef3efdfe0bdc905447abad9f16823dce
Leyang Feng <feng@american.edu>
2018-05-11 14:19:06 -0400

    Added a empty line.

--- code/utility_funs/calBIC.m
+++ code/utility_funs/calBIC.m
@@ -1,7 +1,8 @@
 function bic_remove = calBIC(ortho_err,keep_knots,penalty)
-%BIC acccumulation
-  positive_idx = find(ortho_err>0); %in case a value is exactly 0
-%this line is maybe the most computationally expensive single line in nita
+% BIC acccumulation
+ 
+  positive_idx = ortho_err>0; %in case a value is exactly 0
+% this line is maybe the most computationally expensive single line in nita
   dist_ortho_err = fitdist(ortho_err(positive_idx), 'lognormal');
   loglik = -dist_ortho_err.negloglik; % log likelyhood
   num_segs=length(keep_knots)-1;
