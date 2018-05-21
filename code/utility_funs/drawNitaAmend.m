30018db9839930c06e18ae78daf3cbc6207f8266
Leyang Feng <feng@american.edu>
2018-05-21 15:37:34 -0400

    Added empty string check for the function.

--- code/utility_funs/drawNitaAmend.m
+++ code/utility_funs/drawNitaAmend.m
@@ -40,54 +40,57 @@ loc_idx = find(ismember(draw_objid,redo_objid));
 
 % ---
 %3. redo the hand draw 
-vi_all = table2array(tb(:,{vi_type}));
-for i=1:length(loc_idx)
-    tp_locidx = loc_idx(i);
-    object_id = draw_objid(tp_locidx);
-    obj_idx = find(tb.OBJECTID==object_id); 
+if isempty(loc_idx)
+    disp('No OBJECTID provided to re draw!')
+else
+    vi_all = table2array(tb(:,{vi_type}));
+    for i=1:length(loc_idx)
+        tp_locidx = loc_idx(i);
+        object_id = draw_objid(tp_locidx);
+        obj_idx = find(tb.OBJECTID==object_id); 
     
-    if nargin==8
-        temp = table2array(tb(:,{info_col}));
-        plot_info = char(unique(temp(obj_idx)));
-    else
-        plot_info = '';
-    end 
+        if nargin==8
+            temp = table2array(tb(:,{info_col}));
+            plot_info = char(unique(temp(obj_idx)));
+        else
+            plot_info = '';
+        end 
     
+        im_date = date_num(obj_idx);
+        doy = doy_all(obj_idx);
+        vi = vi_all(obj_idx);
     
-    im_date = date_num(obj_idx);
-    doy = doy_all(obj_idx);
-    vi = vi_all(obj_idx);
+        [im_date,vi,doy] = filterLimits(im_date,vi,doy,value_limits,date_limits,doy_limits);
     
-    [im_date,vi,doy] = filterLimits(im_date,vi,doy,value_limits,date_limits,doy_limits);
-    
-    figure, hold on
-         scatter(im_date, vi, 100, doy,'.')
-         colorbar;
-         axis([min(im_date) max(im_date) value_limits])
-         title([num2str(object_id) ' ' plot_info])
+        figure, hold on
+            scatter(im_date, vi, 100, doy,'.')
+            colorbar;
+            axis([min(im_date) max(im_date) value_limits])
+            title([num2str(object_id) ' ' plot_info])
 
-     [x_draw{tp_locidx}, y_draw{tp_locidx}] = ginput();
+        [x_draw{tp_locidx}, y_draw{tp_locidx}] = ginput();
      
-    close
+        close
     
-    % perform a correction on hand-draw trajectory 
-    % possible corrections are:
-    % (1) correct double click (non unique data pairs)
-    % (2) correct backwards x values (assuming y values are reasonable)
+        % perform a correction on hand-draw trajectory 
+        % possible corrections are:
+        % (1) correct double click (non unique data pairs)
+        % (2) correct backwards x values (assuming y values are reasonable)
 
-    % (1)
-    [~,~,tp_uni_idx] = unique([x_draw{tp_locidx} y_draw{tp_locidx}],'row','stable');
-    x_draw{tp_locidx} = x_draw{tp_locidx}(tp_uni_idx);
-    y_draw{tp_locidx} = y_draw{tp_locidx}(tp_uni_idx);
+        % (1)
+        [~,~,tp_uni_idx] = unique([x_draw{tp_locidx} y_draw{tp_locidx}],'row','stable');
+        x_draw{tp_locidx} = x_draw{tp_locidx}(tp_uni_idx);
+        y_draw{tp_locidx} = y_draw{tp_locidx}(tp_uni_idx);
 
-    % (2)
-    for j=2:length(x_draw{tp_locidx})
-        tp_bw_flag = (x_draw{tp_locidx}(j) - x_draw{tp_locidx}(j-1)) <= 0;
-        if tp_bw_flag
-            x_draw{tp_locidx}(j) = x_draw{tp_locidx}(j-1)+3;
+        % (2)
+        for j=2:length(x_draw{tp_locidx})
+            tp_bw_flag = (x_draw{tp_locidx}(j) - x_draw{tp_locidx}(j-1)) <= 0;
+            if tp_bw_flag
+                x_draw{tp_locidx}(j) = x_draw{tp_locidx}(j-1)+3;
+            end
         end
-    end
     
+    end
 end
 
 end
